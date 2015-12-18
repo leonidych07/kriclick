86a195421db027b98e2b735bbbccf51c308720d6
Sukhbir Khurmi <sukhbirkhurmi@finallysystems.com>
2015-12-18 17:20:52 +0530

    For the STATE Dropdown, add something at the top so no states selected by default.

--- KriclickSln/Kriclick.BLL/CommonBLL.cs
+++ KriclickSln/Kriclick.BLL/CommonBLL.cs
@@ -8,26 +8,44 @@ using System.Threading.Tasks;
 
 namespace Kriclick.BLL
 {
-   public static  class CommonBLL
+    public static class CommonBLL
     {
-        public static List<State> GetStateList()
+        public static List<States> GetStateList()
         {
-            List<State> stateList = new List<State>();
+            List<States> stateList = new List<States>();
             using (KriclickDBEntities dbContext = new KriclickDBEntities())
             {
-                stateList = dbContext.States.OrderBy(x=>x.Abbreviation).ToList();
+                stateList = dbContext.States.Select(t => new States
+                {
+                    StateId = t.StateId,
+                    StateName = t.StateName,
+                    Abbreviation = t.Abbreviation,
+                    CountryId = t.CountryId
+                }).ToList().
+                OrderBy(x => x.Abbreviation).ToList();
             }
+            stateList.Insert(0, new States { StateId = null, StateName = "Select", Abbreviation = "Select", CountryId = null });
             return stateList;
         }
+
 
         public static List<IndustryType> GetIndustryTypeList()
         {
             List<IndustryType> IndustryTypeList = new List<IndustryType>();
             using (KriclickDBEntities dbContext = new KriclickDBEntities())
             {
-                IndustryTypeList = dbContext.IndustryTypes.OrderBy(x=>x.IndustryType1).ToList();
+                IndustryTypeList = dbContext.IndustryTypes.OrderBy(x => x.IndustryType1).ToList();
             }
             return IndustryTypeList;
         }
     }
+
+    public class States
+    {
+        public int? StateId { get; set; }
+        public string StateName { get; set; }
+        public string Abbreviation { get; set; }
+        public short? CountryId { get; set; }
+    }
+
 }
